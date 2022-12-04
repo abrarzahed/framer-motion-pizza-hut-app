eb630200e3a9fa3b274f18fb5e06e3b10ef36888
abrarzahed <abrarzahed6986@gmail.com>
2022-12-05 00:27:12 +0600

    taken up to tranistion

---
+++ src/components/Base.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const Base = ({ addBase, pizza }) => {
+  const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
+
+  return (
+    <motion.div
+      initial={{ x: "100vw" }}
+      animate={{ x: 0 }}
+      transition={{ type: "spring", delay: 0.5 }}
+      className="base container"
+    >
+      <h3>Step 1: Choose Your Base</h3>
+      <ul>
+        {bases.map((base) => {
+          let spanClass = pizza.base === base ? "active" : "";
+          return (
+            <li key={base} onClick={() => addBase(base)}>
+              <span className={spanClass}>{base}</span>
+            </li>
+          );
+        })}
+      </ul>
+
+      {pizza.base && (
+        <motion.div
+          initial={{ x: "-100vw" }}
+          animate={{ x: 0 }}
+          transition={{ type: "spring", stiffness: 120 }}
+          className="next"
+        >
+          <Link to="/toppings">
+            <button>Next</button>
+          </Link>
+        </motion.div>
+      )}
+    </motion.div>
+  );
+};
+
+export default Base;
---
+++ src/components/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const Home = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.5, duration: 1.5 }}
+      className="home container"
+    >
+      <h2>Welcome to Pizza Joint</h2>
+      <Link to="/base">
+        <motion.button animate={{}}>Create Your Pizza</motion.button>
+      </Link>
+    </motion.div>
+  );
+};
+
+export default Home;
