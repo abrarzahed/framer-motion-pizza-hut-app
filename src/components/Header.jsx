e6fd1be6fe9452dcc560e4cd71cec6c3d2f6fa6c
abrarzahed <abrarzahed6986@gmail.com>
2022-12-06 02:00:27 +0600

    completed drag motion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,29 +28,32 @@ const pathVariants = {
 const Header = () => {
   return (
     <header>
-      <Link to="/">
-        <div className="logo">
-          <motion.svg
-            className="pizza-svg"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 100 100"
-            variants={svgVariants}
-            initial="hidden"
-            animate="visible"
-          >
-            <motion.path
-              fill="none"
-              d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
-              variants={pathVariants}
-            />
-            <motion.path
-              fill="none"
-              d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
-              variants={pathVariants}
-            />
-          </motion.svg>
-        </div>
-      </Link>
+      <motion.div
+        drag
+        dragConstraints={{ left: 0, top: 0, right: 0, bottom: 0 }}
+        dragElastic={1}
+        className="logo"
+      >
+        <motion.svg
+          className="pizza-svg"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 100 100"
+          variants={svgVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.path
+            fill="none"
+            d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
+            variants={pathVariants}
+          />
+          <motion.path
+            fill="none"
+            d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
+            variants={pathVariants}
+          />
+        </motion.svg>
+      </motion.div>
 
       <motion.div
         initial={{ y: -255 }}
